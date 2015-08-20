417a6fa2a5cbe3e53fcbab0e7e63e3342ebe8cad
Makoto <FujiMakoto@users.noreply.github.com>
2015-08-20 14:43:40 -0400

    Initial commit

---
+++ Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief		Faker Application Class
+ * @author		<a href='https://www.Makoto.io'>Makoto Fujimoto</a>
+ * @copyright	(c) 2015 Makoto Fujimoto
+ * @package		IPS Social Suite
+ * @subpackage	Faker
+ * @since		20 Aug 2015
+ * @version		
+ */
+ 
+namespace IPS\faker;
+
+/**
+ * Faker Application Class
+ */
+class _Application extends \IPS\Application
+{
+
+}
