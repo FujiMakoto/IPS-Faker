3822169cbdb2b60a84ef9bb7041ca5dc34d5c2db
Makoto <FujiMakoto@users.noreply.github.com>
2015-09-06 02:21:00 -0400

    AdminCP menu generator

--- Application.php
+++ Application.php
@@ -14,25 +14,60 @@ namespace IPS\faker;
 
 /**
  * Faker Application Class
+ * @package IPS\faker
  */
 class _Application extends \IPS\Application
 {
+	/**
+	 * Application icon
+	 *
+	 * @return  string
+	 */
 	public function get__icon()
 	{
 		return 'plus-square';
 	}
 
+	/**
+	 * Dynamic AdminCP menu generator for extensions
+	 *
+	 * @return array
+	 */
 	public function acpMenu()
 	{
-		$menu = parent::acpMenu();
+		$extensions = \IPS\faker\Faker::allExtensions();
 
-		if ( !\IPS\Application::appIsEnabled( 'forums' ) )
+		$menu = array();
+		foreach ( $extensions as $key => $extension )
 		{
-			unset ( $menu['forumgen'] );
+			$splitKey = explode('_', $key);
+
+			/* What app is this extension for? */
+			$app = property_exists( $extension, 'app' ) ? $extension::$app : $splitKey[0];
+
+			/* Make sure the application is enabled */
+			if ( !\IPS\Application::appIsEnabled( $app ) ) {
+				continue;
+			}
+
+			/* Do we need to create a new category for this app? */
+			if ( !in_array($app, $menu) ) {
+				$menu[ $app ] = array();
+			}
+
+			$extName = implode( '_', array_slice($splitKey, 1) );
+			$menu[ $app ][ $extName ] = array(
+				'tab'           => 'faker',
+				'controller'    => 'items',  // @TODO: We need to discern the controller
+				'do'            => "manage&module=generator&extApp={$app}&extension={$extName}",  // @TODO: This is a hack, we're just overriding the module here by re-declaring it
+				'restriction'   => NULL,
+			);
 		}
-		
+
+		/* Append any misc. modules we want to display here */
+		$origMenu = parent::acpMenu();
+		$menu['tools'] = $origMenu['tools'];
+
 		return $menu;
 	}
-
-
 }
